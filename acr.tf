d79307330062f234f6c7ba1122bb45149790b8c1
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-05-24 11:38:53 -0300

    udpate

--- acr.tf
+++ acr.tf
@@ -10,7 +10,7 @@ resource "azurerm_role_assignment" "aks_acr" {
   provider = azurerm.acr
   count    = var.acr_name != "" ? 1 : 0
 
-  principal_id                     = module.cluster.kubelet_identity.principal_id
+  principal_id                     = module.cluster.kubelet_identity.user_assigned_identity_id
   role_definition_name             = var.acr_role_definition_name
   scope                            = data.azurerm_container_registry.acr[0].id
   skip_service_principal_aad_check = var.acr_skip_service_principal_aad_check
--- private_dns_zone.tf
+++ private_dns_zone.tf
@@ -8,7 +8,7 @@ data "azurerm_private_dns_zone" "aks" {
 
 resource "azurerm_role_assignment" "aks_private_dns_zone" {
   provider = azurerm.acr
-  for_each = var.private_dns_zone_enabled ? (contains(var.identity_type, "UserAssigned") ? var.identity_ids : [module.cluster.system_assigned_identity.principal_id]) : []
+  for_each = var.private_dns_zone_enabled ? (try(regex("\\bUserAssigned\\b", var.identity_type), "") == "UserAssigned" ? var.identity_ids : [module.cluster.system_assigned_identity.principal_id]) : []
 
   principal_id                     = each.value
   role_definition_name             = var.private_dns_zone_role_definition_name
