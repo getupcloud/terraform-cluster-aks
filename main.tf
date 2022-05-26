dd9d2b35ddfc51cf0eab1c6d059da9ebb185943b
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-05-25 22:34:12 -0300

    update

--- main.tf
+++ main.tf
@@ -147,3 +147,31 @@ module "cluster" {
   allowed_maintenance_windows     = var.allowed_maintenance_windows
   not_allowed_maintenance_windows = var.not_allowed_maintenance_windows
 }
+
+resource "azurerm_kubernetes_cluster_node_pool" "node_pools" {
+  for_each = local.node_pools
+
+  kubernetes_cluster_id  = module.cluster.aks_id
+  name                   = each.key
+  enable_auto_scaling    = each.value.enable_auto_scaling
+  min_count              = each.value.min_count
+  max_count              = each.value.max_count
+  node_count             = try(each.value.node_count, null)
+  max_pods               = each.value.max_pods
+  vm_size                = each.value.vm_size
+  zones                  = each.value.zones
+  node_labels            = each.value.node_labels
+  node_taints            = each.value.node_taints
+  os_disk_size_gb        = each.value.os_disk_size_gb
+  os_disk_type           = each.value.os_disk_type
+  enable_node_public_ip  = each.value.enable_node_public_ip
+  enable_host_encryption = each.value.enable_host_encryption
+  vnet_subnet_id         = length(data.azurerm_subnet.node_subnet) > 0 ? data.azurerm_subnet.node_subnet[0].id : try(var.default_node_pool.vnet_subnet_id, null)
+  tags                   = merge(var.tags, try(each.value.node_tags, {}))
+
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
