40fd63eb71d8b98b39a45b201d0ed2b1392f1fce
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2024-12-26 19:50:58 +0000

    bump

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "internet" {
 }
 
 module "flux" {
-  source = "github.com/getupcloud/terraform-module-flux?ref=v2.8.1"
+  source = "github.com/getupcloud/terraform-module-flux?ref=v2.8.3"
 
   git_repo                = var.flux_git_repo
   manifests_path          = "./clusters/${var.cluster_name}/aks/manifests"
